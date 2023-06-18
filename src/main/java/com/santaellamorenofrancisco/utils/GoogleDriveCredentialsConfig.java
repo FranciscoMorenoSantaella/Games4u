dfe735a0e12e3ac375c9475bd4dc87d7456ef267
FranciscoMorenoSantaella <santaellamorenofrancisco@gmail.com>
2023-06-18 12:24:42 +0200

    añadiendo jar drive con cambios en el bean

--- src/main/java/com/santaellamorenofrancisco/utils/GoogleDriveCredentialsConfig.java
+++ src/main/java/com/santaellamorenofrancisco/utils/GoogleDriveCredentialsConfig.java
@@ -1,9 +1,21 @@
 package com.santaellamorenofrancisco.utils;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
-import java.io.FileInputStream;
-import java.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+
+@Configuration
 public class GoogleDriveCredentialsConfig {
     private static final String CREDENTIALS_FILE_PATH = "classpath:games4u-backend-f4ea2133dc38.json";
 
@@ -11,6 +23,20 @@ public class GoogleDriveCredentialsConfig {
         return GoogleCredentials.fromStream(
                 GoogleDriveCredentialsConfig.class.getClassLoader().getResourceAsStream(CREDENTIALS_FILE_PATH)
         );
+    }
+    
+    @Bean
+    public Drive drive() throws IOException, GeneralSecurityException {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+                getClass().getResourceAsStream(CREDENTIALS_FILE_PATH)
+        ).createScoped(Collections.singleton(DriveScopes.DRIVE));
+
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        Drive drive = new Drive.Builder(httpTransport, GsonFactory.getDefaultInstance(), new HttpCredentialsAdapter(credentials))
+                .setApplicationName("Games4u")
+                .build();
+
+        return drive;
     }
 }
 

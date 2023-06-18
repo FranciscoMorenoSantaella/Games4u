2090ba6c50559ced0f72d5c45f97eb2e2eafd35f
FranciscoMorenoSantaella <santaellamorenofrancisco@gmail.com>
2023-06-18 16:51:07 +0200

    probando otras cosas

--- src/main/java/com/santaellamorenofrancisco/controller/GoogleDriveController.java
+++ src/main/java/com/santaellamorenofrancisco/controller/GoogleDriveController.java
@@ -3,6 +3,7 @@ package com.santaellamorenofrancisco.controller;
 import com.google.api.client.http.FileContent;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import com.santaellamorenofrancisco.utils.FileUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-
+import java.util.Collections;
 
 @RestController
 public class GoogleDriveController {
 
     private final Drive drive;
+    private static final String FOLDER_NAME = "g4"; // Nombre de la carpeta
 
     @Autowired
     public GoogleDriveController(Drive drive) {
@@ -26,9 +28,17 @@ public class GoogleDriveController {
 
     @PostMapping("/upload")
     public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        // Buscar la carpeta por nombre
+        String folderId = getFolderIdByName(FOLDER_NAME);
+        if (folderId == null) {
+            return "Carpeta no encontrada.";
+        }
+
         // Upload the file to Google Drive
         File fileMetadata = new File();
         fileMetadata.setName(FileUtils.uniqueFileName());
+        fileMetadata.setParents(Collections.singletonList(folderId));
+
         java.io.File tempFile = java.io.File.createTempFile("temp", null);
         file.transferTo(tempFile);
         FileContent mediaContent = new FileContent(file.getContentType(), tempFile);
@@ -41,6 +51,15 @@ public class GoogleDriveController {
 
         return "File uploaded successfully. ID: " + uploadedFile.getId();
     }
+
+    // Buscar el ID de la carpeta por nombre
+    private String getFolderIdByName(String folderName) throws IOException {
+        String query = "mimeType='application/vnd.google-apps.folder' and name='" + folderName + "'";
+        Drive.Files.List request = drive.files().list().setQ(query);
+        FileList fileList = request.execute();
+        if (fileList.getFiles() != null && !fileList.getFiles().isEmpty()) {
+            return fileList.getFiles().get(0).getId();
+        }
+        return null;
+    }
 }
-
-

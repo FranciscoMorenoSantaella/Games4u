f9121ca4b8f487b05106d375e5b14b683c6ac70c
FranciscoMorenoSantaella <santaellamorenofrancisco@gmail.com>
2023-06-18 16:02:13 +0200

    cambiando cosas

--- src/main/java/com/santaellamorenofrancisco/controller/GoogleDriveController.java
+++ src/main/java/com/santaellamorenofrancisco/controller/GoogleDriveController.java
@@ -3,14 +3,18 @@ package com.santaellamorenofrancisco.controller;
 import com.google.api.client.http.FileContent;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import com.santaellamorenofrancisco.utils.FileUtils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 @RestController
 public class GoogleDriveController {
@@ -23,29 +27,23 @@ public class GoogleDriveController {
     }
 
     @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    public String uploadFile(@RequestPart("file") MultipartFile file) throws IOException {
         if (file != null && !file.isEmpty()) {
-            // Obtiene el nombre original del archivo
-            String originalFilename = file.getOriginalFilename();
-            
-            // Crea los metadatos del archivo
+            // Create the file metadata
             File fileMetadata = new File();
-            fileMetadata.setName("hola");
-            
-            // Crea un archivo temporal para transferir el contenido del archivo subido
-            java.io.File tempFile = java.io.File.createTempFile("temp", null);
-            file.transferTo(tempFile);
-            
-            // Crea el contenido del archivo para la subida
-            FileContent mediaContent = new FileContent(file.getContentType(), tempFile);
-            
-            // Crea el archivo en Google Drive
-            File uploadedFile = drive.files().create(fileMetadata, mediaContent)
+            fileMetadata.setName(FileUtils.uniqueFileName());
+
+            // Create a temporary file to store the uploaded file
+            Path tempFilePath = Files.createTempFile("temp", null);
+            Files.copy(file.getInputStream(), tempFilePath, StandardCopyOption.REPLACE_EXISTING);
+
+            // Upload the file to Google Drive
+            File uploadedFile = drive.files().create(fileMetadata, new FileContent(file.getContentType(), tempFilePath.toFile()))
                     .setFields("id")
                     .execute();
 
-            // Borra el archivo temporal
-            tempFile.delete();
+            // Delete the temporary file
+            Files.delete(tempFilePath);
 
             return "File uploaded successfully. ID: " + uploadedFile.getId() + ", Name: " + uploadedFile.getName();
         } else {
